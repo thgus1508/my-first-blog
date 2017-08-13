701e68f364ceff5e6d0a31f6659c7a7aebd48211
thgus1508 <wlgus243@naver.com>
2017-08-13 23:48:41 +0900

    added Login

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^login/$', views.user_login, name='login'),
+
+
+
 ]
